5ec67a1dec20d2513ded11ee80933bde4dcd9872
HanssenGLRP <66432612+HanssenRP@users.noreply.github.com>
2026-03-15 01:17:21 +0100

    PIpChat
    - Add webhooks for discord logging

--- [LB]/lb-picchat/server/apiKeys.lua
+++ [LB]/lb-picchat/server/apiKeys.lua
@@ -1,3 +1,3 @@
 -- LOG_WEBHOOK = "https://discord.com/api/webhooks/"
-LOG_WEBHOOK = "https://discord.com/api/webhooks/"
+LOG_WEBHOOK = "https://discord.com/api/webhooks/1482528173076906202/5zTlnjI2PrYaB0bgdJ-G4gTu-LIrepvIe3SuVwPJfpiGpnu46TGOzvU9K7jTN6ULjv1C"
 DISCORD_TOKEN = nil
